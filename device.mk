10bd85c6ab9b1ed9d997bffd5526fca68d83ab5b
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    lyriq: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: CloudedQuartz <ayan.pande06@gmail.com>

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
