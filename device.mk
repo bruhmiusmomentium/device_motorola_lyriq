3d1e6ce803a902317462ba417482d91c8903e9ad
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    lyriq: Enable updatable apexes
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: CloudedQuartz <ayan.pande06@gmail.com>

--- device.mk
+++ device.mk
@@ -8,6 +8,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # API level
 PRODUCT_SHIPPING_API_LEVEL := 31
 PRODUCT_EXTRA_VNDK_VERSIONS := $(PRODUCT_SHIPPING_API_LEVEL)
