4ae10cb79e0d73899df2cde56feb24eb1416e163
CloudedQuartz <ayan.pande06@gmail.com>
2023-06-05 11:18:59 +0530

    lyriq: set platform
    Signed-off-by: CloudedQuartz <ayan.pande06@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,3 +6,8 @@ DEVICE_PATH := device/motorola/lyriq
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := lyriq
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6893
+TARGET_BOARD_PLATFORM_GPU := mali-g77mc9
