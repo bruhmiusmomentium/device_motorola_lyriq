43229820801863c8559e29dc45cca9d900222c31
CloudedQuartz <ayan.pande06@gmail.com>
2023-06-05 11:24:20 +0530

    lyriq: set shipping API level
    - mt6891 is on android S (31) vendor
    
    Signed-off-by: CloudedQuartz <ayan.pande06@gmail.com>

--- device.mk
+++ device.mk
@@ -2,6 +2,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# API level
+PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_EXTRA_VNDK_VERSIONS := $(PRODUCT_SHIPPING_API_LEVEL)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
