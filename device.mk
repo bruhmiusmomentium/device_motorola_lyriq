db61bef3eb643b8bfed351cb015127c10a72f2ea
CloudedQuartz <ayan.pande06@gmail.com>
2023-06-06 06:02:08 +0530

    lyriq: build fstab as package
    Signed-off-by: CloudedQuartz <ayan.pande06@gmail.com>

--- device.mk
+++ device.mk
@@ -31,13 +31,11 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Fstab
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.mt6893:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.mt6893 \
-    $(LOCAL_PATH)/rootdir/etc/fstab.emmc:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.emmc
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.mt6893:$(TARGET_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/etc/fstab.mt6893 \
-    $(LOCAL_PATH)/rootdir/etc/fstab.emmc:$(TARGET_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/etc/fstab.emmc
+PRODUCT_PACKAGES += \
+    fstab.mt6893 \
+    fstab.emmc \
+    fstab.mt6893_ramdisk \
+    fstab.emmc_ramdisk
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
